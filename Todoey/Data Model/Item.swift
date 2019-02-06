4d993ea3820f38c970b3b5740f0deb07652d46fb
Alfredo Mendoza <almendoza25@gmail.com>
2019-02-06 09:42:21 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alfredo Mendoza on 2/6/19.
+//  Copyright © 2019 Alfredo Mendoza. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
